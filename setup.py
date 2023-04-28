8b774f655134850663bc2b93711abb35820371ca
Anastasia <margaret-26@mail.ru>
2023-04-29 01:16:17 +0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+sources = []
+name = 'more_numbers'
+description = 'A module for working with numbers in different number systems with long arithmetic.'
+module1 = Extension(name, sources=sources)
+
+setup(
+    name=name,
+    version='1.0',
+    description=description,
+    ext_modules=[module1]
+)
+
+# python setup.py install
